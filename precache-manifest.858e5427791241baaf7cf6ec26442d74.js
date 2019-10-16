7f526ba29d5f272fc7d5a594229ad5bdbe9a6662
Dan Miller <dmm4613@gmail.com>
2019-10-16 16:24:40 -0400

    Updates

---
+++ precache-manifest.858e5427791241baaf7cf6ec26442d74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41490a3a191a517923b9e6bed1b09b65",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "371aead3f25aa5258992",
+    "url": "/monsters-rolodex/static/css/main.5b810bea.chunk.css"
+  },
+  {
+    "revision": "ca903edc950d52328219",
+    "url": "/monsters-rolodex/static/js/2.e124cbd2.chunk.js"
+  },
+  {
+    "revision": "371aead3f25aa5258992",
+    "url": "/monsters-rolodex/static/js/main.a0e63b2d.chunk.js"
+  },
+  {
+    "revision": "d71975295fe45a6cbab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.4686dfee.js"
+  }
+]);
